86803a4da384da3c3df701fe2edf65db84680446
Uros Beric <bu190051d@student.etf.bg.ac.rs>
2022-06-19 13:15:46 +0200

    changed port in configuration.py

--- authentication/configuration.py
+++ authentication/configuration.py
@@ -3,7 +3,7 @@ from datetime import timedelta;
 
 
 class Configuration:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost:3307/authentication"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost:3306/authentication"
     JWT_SECRET_KEY = "JWT_SECRET_KEY"
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
