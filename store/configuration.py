28cec371620e458227dc94cef1090658bc754cf7
Uros Beric <bu190051d@student.etf.bg.ac.rs>
2022-06-19 21:32:50 +0200

    initialized store

---
+++ store/configuration.py
@@ -0,0 +1,11 @@
+
+
+class Configuration:
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost:5003/store"
+    REDIS_HOST = "localhost"
+    REDIS_THREADS_LIST = "threads"
+    JWT_SECRET_KEY = "JWT_SECRET_KEY"
+
+
+
+
