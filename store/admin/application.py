fe22bddf8386d4bd7ddc032d70c0c4589cd51156
Uros Beric <bu190051d@student.etf.bg.ac.rs>
2022-06-20 10:39:14 +0200

    made application.py for every container in store

--- store/admin/application.py
+++ store/admin/application.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, Response, jsonify
-from configuration import Configuration
-from models import database
+from store.configuration import Configuration
+from store.models import database
 from email.utils import parseaddr
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, \
     create_refresh_token, get_jwt, get_jwt_identity
@@ -18,4 +18,4 @@ def index():
 
 if __name__ == "__main__":
     database.init_app(application)
-    application.run(debug=True, host="0.0.0.0", port=5001)
+    application.run(debug=True, host="0.0.0.0", port=5003)
---
+++ store/customer/application.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, Response, jsonify
+from store.configuration import Configuration
+from store.models import database
+from email.utils import parseaddr
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, \
+    create_refresh_token, get_jwt, get_jwt_identity
+from sqlalchemy import and_
+
+application = Flask(__name__)
+application.config.from_object(Configuration)
+jwt = JWTManager(application)
+
+
+@application.route("/", methods=["GET"])
+def index():
+    return "hi"
+
+
+if __name__ == "__main__":
+    database.init_app(application)
+    application.run(debug=True, host="0.0.0.0", port=5004)
