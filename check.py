618fb265766847374020ee053e6ae1e704c78144
Uros Beric <bu190051d@student.etf.bg.ac.rs>
2022-06-20 21:42:29 +0200

    added role check

---
+++ check.py
@@ -0,0 +1,17 @@
+from functools import wraps
+from flask_jwt_extended import verify_jwt_in_request, get_jwt
+from flask import Response
+
+
+def role_check(role):
+    def inner_role(function):
+        @wraps(function)
+        def decorator(*arguments, **keyword_arguments):
+            verify_jwt_in_request()
+            claims = get_jwt()
+            if ("roles" in claims) and (role in claims["roles"]):
+                return function(*arguments, **keyword_arguments)
+            else:
+                return Response(status=403)
+        return decorator
+    return inner_role
