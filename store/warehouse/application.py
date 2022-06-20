0cb419c8d7ea9db7ce15bef6227aa92c3f333a09
Uros Beric <bu190051d@student.etf.bg.ac.rs>
2022-06-20 14:10:53 +0200

    added file reader in update in warehouse

--- store/warehouse/application.py
+++ store/warehouse/application.py
@@ -5,6 +5,9 @@ from email.utils import parseaddr
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, \
     create_refresh_token, get_jwt, get_jwt_identity
 from sqlalchemy import and_
+from store.messages import *
+import io
+import csv
 
 application = Flask(__name__)
 application.config.from_object(Configuration)
@@ -16,8 +19,39 @@ def index():
     return "hi"
 
 
-@application.route("/update", methods=["GET"])
+@application.route("/update", methods=["POST"])
+@jwt_required(refresh=False)
 def update():
+    keys = request.files.keys()
+    if not ("file" in keys):
+        return responseMessageJson(MESSAGE_FIELD_IS_MISSING, "file")
+
+    file = request.files["file"]
+    if not file:
+        responseMessageJson(MESSAGE_FIELD_IS_MISSING, "file")
+
+    content = file.stream.read().decode("utf-8")
+    stream = io.StringIO(content)
+    reader = csv.reader(stream)
+
+    rows = []
+    for row in reader:
+        rows.append(row)
+
+    for i in range(len(rows)):
+        if len(rows[i]) != 4:
+            return responseMessageJson(MESSAGE_INCORRECT_NUMBER_VALUES, str(i))
+
+    for i in range(len(rows)):
+        if int(rows[i][2]) <= 0:
+            return responseMessageJson(MESSAGE_INCORRECT_QUANTITY, str(i))
+
+    for i in range(len(rows)):
+        if float(rows[i][3]) <= 0:
+            return responseMessageJson(MESSAGE_INCORRECT_PRICE, str(i))
+
+
+
     return "TODO"
 
 
