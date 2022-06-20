fb4762f4b377774f09b298b9e0b3e0e0e7f77919
Uros Beric <bu190051d@student.etf.bg.ac.rs>
2022-06-20 19:55:02 +0200

    done customer search

--- store/customer/application.py
+++ store/customer/application.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, Response, jsonify
 from store.configuration import Configuration
-from store.models import database
+from store.models import database, Order, ProductOrder, Product, ProductCategory, Category
 from email.utils import parseaddr
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, \
     create_refresh_token, get_jwt, get_jwt_identity
@@ -18,7 +18,29 @@ def index():
 
 @application.route("/search", methods=["GET"])
 def search():
-    return "TODO"
+    name = request.args.get("name", "")
+    category = request.args.get("category", "")
+
+    categories = Category.query.filter(Category.name.like(f"%{category}%")).all()
+    products = Product.query.join(ProductCategory).join(Category).filter(
+        and_(
+            Product.name.like(f"%{name}%"),
+            Category.name.like(f"%{category}%"),
+        )
+    ).all()
+
+    jsonCategories = []
+    for elem in categories:
+        jsonCategories.append(elem.name)
+    jsonCategories.sort()
+
+    jsonProducts = []
+    for product in products:
+        data = {"categories": [elem.name for elem in product.categories], "id": product.id, "name": product.name,
+                "price": product.price, "quantity": product.quantity}
+        jsonProducts.append(data)
+
+    return jsonify(categories=jsonCategories, products=jsonProducts), 200
 
 
 @application.route("/order", methods=["POST"])
