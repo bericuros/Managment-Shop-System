178f9c702b91b874e32e3de909b23da401083639
Uros Beric <bu190051d@student.etf.bg.ac.rs>
2022-06-21 18:03:23 +0200

    store customer container

--- store/customer/application.py
+++ store/customer/application.py
@@ -1,8 +1,8 @@
 import datetime
 
 from flask import Flask, request, Response, jsonify
-from store.configuration import Configuration
-from store.models import database, Order, ProductOrder, Product, ProductCategory, Category
+from configuration import Configuration
+from models import database, Order, ProductOrder, Product, ProductCategory, Category
 from email.utils import parseaddr
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, \
     create_refresh_token, get_jwt, get_jwt_identity
