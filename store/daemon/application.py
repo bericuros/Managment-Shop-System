8164bf79abf31d20a0ef80317c483b910e779f50
Uros Beric <bu190051d@student.etf.bg.ac.rs>
2022-06-21 15:21:15 +0200

    all tests passed; 60.0/60.0

--- store/daemon/application.py
+++ store/daemon/application.py
@@ -69,10 +69,3 @@ if __name__ == "__main__":
                     if product.quantity == 0:
                         break
                 database.session.commit()
-
-
-
-
-
-
-    # application.run(debug=True, host="0.0.0.0", port=5005)
