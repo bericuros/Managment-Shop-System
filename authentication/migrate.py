a11400d812e740759fe1a7c693428d64073bef6e
Uros Beric <bu190051d@student.etf.bg.ac.rs>
2022-06-22 12:33:54 +0200

    removed healthcheck and changed migrations

--- authentication/migrate.py
+++ authentication/migrate.py
@@ -3,54 +3,57 @@ from configuration import Configuration
 from flask_migrate import Migrate, init, migrate, upgrade
 from models import database, Role, UserRole, User
 from sqlalchemy_utils import database_exists, create_database
+import shutil
 
 application = Flask(__name__)
 application.config.from_object(Configuration)
 
 migrateObject = Migrate(application, database)
 
-if not database_exists(Configuration.SQLALCHEMY_DATABASE_URI):
-    create_database(Configuration.SQLALCHEMY_DATABASE_URI)
+done = False
 
-database.init_app(application)
-
-with application.app_context() as context:
+while not done:
     try:
-        init()
-    except:
-        dummy = 0
-    try:
-        migrate(message="Initial migration.")
-    except:
-        dummy = 0
-    try:
-        upgrade()
-    except:
-        dummy = 0
+        if not database_exists(Configuration.SQLALCHEMY_DATABASE_URI):
+            create_database(Configuration.SQLALCHEMY_DATABASE_URI)
 
-    adminRole = Role(name="admin")
-    customerRole = Role(name="customer")
-    warehouseRole = Role(name="warehouse")
+        database.init_app(application)
 
-    database.session.add(adminRole)
-    database.session.add(customerRole)
-    database.session.add(warehouseRole)
-    database.session.commit()
+        with application.app_context() as context:
+            try:
+                shutil.rmtree("migrations")
+            except Exception as error:
+                print(error)
+            init()
+            migrate(message="Initial migration.")
+            upgrade()
+            adminRole = Role(name="admin")
+            customerRole = Role(name="customer")
+            warehouseRole = Role(name="warehouse")
 
-    admin = User(
-        email="admin@admin.com",
-        password="1",
-        forename="admin",
-        surname="admin"
-    )
+            database.session.add(adminRole)
+            database.session.add(customerRole)
+            database.session.add(warehouseRole)
+            database.session.commit()
 
-    database.session.add(admin)
-    database.session.commit()
+            admin = User(
+                email="admin@admin.com",
+                password="1",
+                forename="admin",
+                surname="admin"
+            )
 
-    userRole = UserRole(
-        userId=admin.id,
-        roleId=adminRole.id
-    )
+            database.session.add(admin)
+            database.session.commit()
 
-    database.session.add(userRole)
-    database.session.commit()
+            userRole = UserRole(
+                userId=admin.id,
+                roleId=adminRole.id
+            )
+
+            database.session.add(userRole)
+            database.session.commit()
+
+            done = True
+    except Exception as error:
+        print(error)
--- store/migrate.py
+++ store/migrate.py
@@ -3,27 +3,30 @@ from configurationDatabase import Configuration
 from flask_migrate import Migrate, init, migrate, upgrade
 from models import database
 from sqlalchemy_utils import database_exists, create_database
+import shutil
 
 application = Flask(__name__)
 application.config.from_object(Configuration)
 
 migrateObject = Migrate(application, database)
 
-if not database_exists(Configuration.SQLALCHEMY_DATABASE_URI):
-    create_database(Configuration.SQLALCHEMY_DATABASE_URI)
+done = False
 
-database.init_app(application)
+while not done:
+    try:
+        if not database_exists(Configuration.SQLALCHEMY_DATABASE_URI):
+            create_database(Configuration.SQLALCHEMY_DATABASE_URI)
 
-with application.app_context() as context:
-    try:
-        init()
-    except:
-        dummy = 0
-    try:
-        migrate(message="Initial migration.")
-    except:
-        dummy = 0
-    try:
-        upgrade()
-    except:
-        dummy = 0
+        database.init_app(application)
+
+        with application.app_context() as context:
+            try:
+                shutil.rmtree("migrations")
+            except Exception as error:
+                print(error)
+            init()
+            migrate(message="Initial migration.")
+            upgrade()
+            done = True
+    except Exception as error:
+        print(error)
